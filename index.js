09948cfc6d1e31733764a9c26ab41f7c04568bcf
jimmy-huang <jimmy.huang@binance.com>
2020-05-05 05:29:13 +0800

    feat: 在第一次自动安装依赖

--- index.js
+++ index.js
@@ -2,33 +2,67 @@
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
+execSync(`cd ${__dirname}; yarn`)
+const chalk = require('chalk')
 
-const copyFolder = (fromPath, toPath = 'playground', direct) => {
-  if (!direct) {
+const calcPath = (path) => {
+  let isExists = fs.existsSync(path)
+  if (isExists) {
+    const reg = /\d+$/
+    const num = path.match(reg)
+    if (num) return calcPath(`${path.substring(0, num.index)}${parseInt(num) + 1}`)
+
+    return calcPath(`${path}1`)
+  }
+
+  return path
+}
+
+const createDir = (dirPath) => {
+  fs.mkdirSync(dirPath)
+}
+
+const copyFolder = (fromPath, _toPath = 'playground', notFirstTime) => {
+  const toPath = calcPath(_toPath)
+
+  // 第一次执行copyFolder
+  if (!notFirstTime) {
     fromPath = path.join(__dirname, fromPath)
-    // toPath = path.join(__dirname, toPath)
+    if (toPath !== _toPath) {
+      console.log(
+        chalk.yellow(
+          `Dir ${chalk.bold.italic(_toPath)} is exists. Creating dir ${chalk.bold.italic(toPath)}.`,
+        ),
+      )
+    } else {
+      console.log(chalk.green(`Creating dir ${chalk.bold.italic(toPath)}`))
+    }
   }
-  const createDir = (dirPath) => {
-    fs.mkdirSync(dirPath)
-  }
-  if (fs.existsSync(fromPath)) {
-    createDir(toPath)
 
-    const filesOrDirs = fs.readdirSync(fromPath, { withFileTypes: true })
-    for (let i = 0; i < filesOrDirs.length; i++) {
-      const targetFileOrDir = filesOrDirs[i]
-      const source = path.join(fromPath, targetFileOrDir.name)
-      const target = path.join(toPath, targetFileOrDir.name)
-      if (targetFileOrDir.isFile()) {
-        const buffer = fs.readFileSync(source)
-        fs.writeFileSync(target, buffer)
-      } else {
-        copyFolder(source, target, true)
-      }
+  createDir(toPath)
+
+  const filesOrDirs = fs.readdirSync(fromPath, { withFileTypes: true })
+  for (let i = 0; i < filesOrDirs.length; i++) {
+    const targetFileOrDir = filesOrDirs[i]
+    const source = path.join(fromPath, targetFileOrDir.name)
+    const target = path.join(toPath, targetFileOrDir.name)
+    if (targetFileOrDir.isFile()) {
+      const buffer = fs.readFileSync(source)
+      fs.writeFileSync(target, buffer)
+    } else {
+      copyFolder(source, target, true)
     }
   }
 }
 
 copyFolder('./filesToCopy')
 
-execSync('cd playground; yarn')
+// execSync('cd playground; yarn')
+
+// // 已经安装好依赖了，清空无用的东西
+// const indexPath = path.join(__dirname, 'index.js')
+// const buffer = fs.readFileSync(indexPath)
+// const str = buffer.toString()
+// const reg = /execSync\(`cd \${__dirname}; yarn`\)\s*|(?<=execSync\('cd playground; yarn'\))[\s\S]*/g
+// const finalStr = str.replace(reg, '')
+// fs.writeFileSync(indexPath, finalStr)
